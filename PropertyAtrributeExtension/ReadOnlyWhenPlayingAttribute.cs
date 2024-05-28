8d29ca6dd985b7fd1460ebd3176fd3abc6dc98ff
Khiew Jian Bin <khiewbin@gmail.com>
2024-05-29 01:29:48 +0800

    Init + Added ReadOnly Custom Properpty Attribute

---
+++ PropertyAtrributeExtension/ReadOnlyWhenPlayingAttribute.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ReadOnlyWhenPlayingAttribute))]
+public class ReadOnlyWhenPlayingDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = !Application.isPlaying;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
+#endif
+
+public class ReadOnlyWhenPlayingAttribute : PropertyAttribute { }
